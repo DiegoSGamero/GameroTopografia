0765ef3dd784b0537555a9d9e3a61f29aa81a204
diego joao <diegosgjoao@gmail.com>
2024-01-16 10:24:01 -0300

    made some validations and association between users and properties

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,11 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :full_name, presence: true, format: { with: /\A[a-zA-Z]+\.?(\s[a-zA-Z]+\.?)+\z/, message: 'O nome completo deve ter pelo menos 2 nomes' }
+  validates :email, presence: true, uniqueness: true
+
+  validates :password, format: { with: /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}\z/, message: 'Deve conter pelo menos uma letra e um número' }
+
+  validates :username, presence: true, uniqueness: true, format: { with: /\A(?=.*gamero)(?=.*\d)(?=.*[!@#\$%^&*()-_=+{}\[\]|\\;:'",.<>\/\?`~]).*\z/ }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_13_165111) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_16_132125) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_13_165111) do
     t.string "CAR"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_properties_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +59,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_13_165111) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "properties", "users"
 end
