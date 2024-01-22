79f9bbcc554d01b113e23b4129283a5697453803
diego joao <diegosgjoao@gmail.com>
2024-01-22 17:27:06 -0300

    admins devise configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+    registrations: 'admins/registrations',
+    sessions: 'admins/sessions'
+  }
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
