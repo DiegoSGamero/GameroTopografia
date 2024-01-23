d9d0c5aaf122b4f960b546125445190319c592ee
diego joao <diegosgjoao@gmail.com>
2024-01-23 09:58:53 -0300

    property's controller created

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,16 @@
 class Property < ApplicationRecord
   belongs_to :user
+
+  validates :address, presence: true
+  validates :owners, presence: { message: 'Precisamos de pelo menos um proprietário declarado' }
+  validate :at_least_one_owner
+  validates :size, presence: true
+  validates :limit_type, presence: true
+  validates :registration_number, presence: true, numericality: true
+
+  private
+
+  def at_least_one_owner
+    errors.add(:base, 'Precisamos de pelo menos um proprietário declarado') if owners.blank?
+  end
 end
