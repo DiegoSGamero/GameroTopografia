17cee5037672003a25f9e36fea893197757d7d3d
diego joao <diegosgjoao@gmail.com>
2024-01-25 15:55:51 -0300

    advancing with property sprint

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,14 +3,9 @@ class Property < ApplicationRecord
 
   validates :address, presence: true
   validates :owners, presence: { message: 'Precisamos de pelo menos um proprietário declarado' }
-  validate :at_least_one_owner
   validates :size, presence: true
   validates :limit_type, presence: true
   validates :registration_number, presence: true, numericality: true
 
-  private
 
-  def at_least_one_owner
-    errors.add(:base, 'Precisamos de pelo menos um proprietário declarado') if owners.blank?
-  end
 end
