da66b960e8865205673b477d89bf7ab27e4cfa2e
diego joao <diegosgjoao@gmail.com>
2024-02-29 14:46:11 -0300

    understanding logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 require "active_support/core_ext/integer/time"
+logger.info "Loading production.rb file..."
+
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
