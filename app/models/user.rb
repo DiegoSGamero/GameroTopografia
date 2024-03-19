3c4df37a3cc54f0247e931c0620675d59b0452bd
diego joao <diegosgjoao@gmail.com>
2024-03-19 16:08:59 -0300

    users validates ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   has_many :properties
   has_many_attached :photos, service: :cloudinary, dependent: :purge
 
-  validates :full_name, presence: true, format: { with: /\A[a-zA-Z]+\.?(\s[a-zA-Z]+\.?)+\z/, message: "O nome completo deve ter pelo menos 2 nomes" }
+  validates :full_name, presence: true, format: { with: /\A[a-zA-ZÀ-ú]+\.?(\s[a-zA-ZÀ-ú]+\.?)+\z/, message: "O nome completo deve ter pelo menos 2 nomes" }
   validates :email, presence: true, uniqueness: true
-  validates :RG, presence: true, uniqueness: true, format: { with: /\A\d{7,9}\z/, message: "O RG deve ter entre 7 e 9 dígitos" }
+  validates :RG, presence: true, uniqueness: true, format: { with: /\A\d{7,9}[0-9X]?\z/, message: "O RG deve ter entre 7 e 9 dígitos" }
   validates :CPF, presence: true, uniqueness: true, format: { with: /\A\d{11}\z/, message: "O CPF deve ter exatamente 11 dígitos" }
   validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: "O número de telefone deve ter entre 10 e 11 dígitos" }
   validates :address, presence: true
