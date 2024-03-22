6b7c6238043ecba512ac37eae42ef432f3d50de4
diego joao <diegosgjoao@gmail.com>
2024-03-22 19:02:02 -0300

    some production configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'http://gamerotopografia-production.up.railway.app' }
+  config.action_mailer.default_url_options = { host: 'gamerotopografia-production.up.railway.app' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
