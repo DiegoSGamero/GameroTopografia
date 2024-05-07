9c2914b981df9997adcb15f5ae643a3aefdd71ce
diego joao <diegosgjoao@gmail.com>
2024-05-07 17:07:07 -0300

    some adjustments in js

--- app/javascript/controllers/_carousel.js
+++ app/javascript/controllers/_carousel.js
@@ -1,37 +1,71 @@
-const controls = document.querySelectorAll('.control')
+// const controls = document.querySelectorAll('.control')
 
-let currentItem = 0;
+// let currentItem = 0;
 
-const items = document.querySelectorAll('.item');
+// const items = document.querySelectorAll('.item');
 
-const maxItems = items.length;
+// const maxItems = items.length;
 
-controls.forEach(control => {
-  control.addEventListener('click', () => {
-    const isLeft = control.classList.contains('arrow-left');
+// controls.forEach(control => {
+//   control.addEventListener('click', () => {
+//     const isLeft = control.classList.contains('arrow-left');
 
-    if (isLeft) {
-      currentItem -= 1;
-    } else {
-      currentItem += 1;
-    }
+//     if (isLeft) {
+//       currentItem -= 1;
+//     } else {
+//       currentItem += 1;
+//     }
 
-    if (currentItem >= maxItems) {
-      currentItem = 0;
-    }
+//     if (currentItem >= maxItems) {
+//       currentItem = 0;
+//     }
 
-    if (currentItem < 0) {
-      currentItem = maxItems - 1;
-    }
+//     if (currentItem < 0) {
+//       currentItem = maxItems - 1;
+//     }
 
-    // console.log("control", isLeft, currentItem);
+//     // console.log("control", isLeft, currentItem);
 
-    // items.forEach(item => {
+//     // items.forEach(item => {
+//       items[currentItem].scrollIntoView({
+//         // inline: "center",
+//         block: "center",
+//         behavior: "smooth",
+//       });
+//     // });
+//   })
+// });
+document.addEventListener('turbo:load', () => {
+  const controls = document.querySelectorAll('.control');
+  let currentItem = 0; // Reset currentItem on each load
+
+  const items = document.querySelectorAll('.item');
+  const maxItems = items.length;
+
+  controls.forEach(control => {
+    control.addEventListener('click', () => {
+      const isLeft = control.classList.contains('arrow-left');
+
+      if (isLeft) {
+        currentItem -= 1;
+      } else {
+        currentItem += 1;
+      }
+
+      if (currentItem >= maxItems) {
+        currentItem = 0;
+      }
+
+      if (currentItem < 0) {
+        currentItem = maxItems - 1;
+      }
+
+      // console.log("control", isLeft, currentItem);
+
       items[currentItem].scrollIntoView({
-        // inline: "center",
         block: "center",
         behavior: "smooth",
       });
-    // });
-  })
+    });
+  });
 });
--- app/javascript/controllers/_knowledge.js
+++ app/javascript/controllers/_knowledge.js
@@ -1,4 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
+//   const buttons = document.querySelectorAll(".knowledge-topics .btn");
+//   const sections = document.querySelectorAll(".knowledge-texts > div");
+
+//   buttons.forEach(function(button, index) {
+//     button.addEventListener("click", function() {
+//       sections[index].scrollIntoView({ behavior: "smooth" });
+//     });
+//   });
+// });
+document.addEventListener('turbo:load', () => {
   const buttons = document.querySelectorAll(".knowledge-topics .btn");
   const sections = document.querySelectorAll(".knowledge-texts > div");
 
